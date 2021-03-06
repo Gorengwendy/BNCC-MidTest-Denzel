0fc84ee7ac3f1493adfa759eda4264e443f095dc
Denzel <Denzel.wendy@binus.ac.id>
2021-03-06 18:27:04 +0700

    Memperbaiki Edit.blade

--- blog/resources/views/edit.blade.php
+++ blog/resources/views/edit.blade.php
@@ -20,13 +20,13 @@
         </div>
 
         <div class="form-group">
-            <label for="halaman">Deskripsi</label>
+            <label for="halaman">Halaman</label>
             <input type="number" value="{{ $article->halaman }}" name="halaman" class="form-control" id="halaman"
                 placeholder="Jumlah Halaman">
         </div>
 
         <div class="form-group">
-            <label for="tahun">Deskripsi</label>
+            <label for="tahun">Tahun Terbit</label>
             <input type="number" value="{{ $article->tahun }}" name="tahun" class="form-control" id="tahun"
                 placeholder="Tahun Terbit" min="1990" max="2022">
         </div>
@@ -53,9 +53,9 @@
     </script>
 
     <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-        -->
+                                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+                                <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+                                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+                                -->
 
 @endsection
